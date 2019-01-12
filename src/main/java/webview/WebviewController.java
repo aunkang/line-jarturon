16882a9070e11c88f610cf9c9d66cad2988409b0
Jatoron <jatorun@gmail.com>
2019-01-13 00:35:36 +0700

    add requestParam

--- src/main/java/webview/WebviewController.java
+++ src/main/java/webview/WebviewController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import linebot.Application;
@@ -13,7 +14,7 @@ import linebot.Task;
 public class WebviewController {
 
 	@RequestMapping(name = "/getTask", method = { RequestMethod.GET })
-	public List<Task> getTasks(String userId) {
+	public List<Task> getTasks(@RequestParam("userId") String userId) {
 		return Application.maps.get(userId);
 	}
 

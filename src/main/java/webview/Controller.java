b3c4242a968af2f1c679037415e12e11a0e1e8e5
Jatoron <jatorun@gmail.com>
2019-01-12 22:45:18 +0700

    add controller

--- src/main/java/webview/Controller.java
+++ src/main/java/webview/Controller.java
@@ -1,5 +1,19 @@
 package webview;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import linebot.Application;
+import linebot.Task;
+
+@RestController
 public class Controller {
+	
+	@GetMapping("/webview")
+	public List<Task> getTasks() {
+		return null;
+	}
 	
 }

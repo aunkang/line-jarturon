19618f2126314ce8ceab7c8e206bf3c5952b242e
aunkang <aunkang@gmail.com>
2019-01-12 23:42:44 +0700

    remove controller

--- src/main/java/webview/Controller.java
+++ src/main/java/webview/Controller.java
@@ -7,13 +7,13 @@ import org.springframework.web.servlet.ModelAndView;
 @RestController
 public class Controller {
 	
-	@GetMapping("/webview")
-	public ModelAndView getTasks() {
-		ModelAndView mav = new ModelAndView("viewtask.html");
-		System.err.println("Hello, controller!");
-		System.out.println("Hello, controller!");
-		
-		return mav;
-	}
+//	@GetMapping("/webview")
+//	public ModelAndView getTasks() {
+//		ModelAndView mav = new ModelAndView("viewtask.html");
+//		System.err.println("Hello, controller!");
+//		System.out.println("Hello, controller!");
+//		
+//		return mav;
+//	}
 	
 }

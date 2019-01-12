3e6acb1e304269eb3a7c38454e74df7657176f3c
Jatoron <jatorun@gmail.com>
2019-01-13 00:01:02 +0700

    add request mapping

--- src/main/java/webview/WebviewController.java
+++ src/main/java/webview/WebviewController.java
@@ -1,13 +1,14 @@
 package webview;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class WebviewController {
 
-	@GetMapping("/webview")
+	@RequestMapping(name = "/webview", method = { RequestMethod.GET, RequestMethod.POST })
 	public ModelAndView getTasks() {
 		ModelAndView mav = new ModelAndView("viewtask.html");
 		System.err.println("Hello, controller!");

32d13f0dc41051252ac1c43dbdd912c1abd42486
aunkang <aunkang@gmail.com>
2019-01-13 18:55:07 +0700

    Update response msg

--- src/main/java/linebot/Application.java
+++ src/main/java/linebot/Application.java
@@ -91,7 +91,7 @@ public class Application {
 		this.saveTask(userId, task);
 		
 		return new TextMessage("Your task has been added" + "    \n\n" + "Name: " + taskStr + "    \n" + "Date: " + dueDate
-				+ "    \n" + "HH:mm : " + hhStr + ":" + mmStr + "\n\n See all task click link below. \n line://app/1613138841-EqAkLo6L" + "\n\n " + e.getTimestamp().getEpochSecond()+ "\n\n " + e.getTimestamp().getNano());
+				+ "    \n" + "HH:mm : " + hhStr + ":" + mmStr + "\n\n See all task click link below. \n line://app/1613138841-EqAkLo6L" + "\n\n " + dateStr);
 	}
 
 	private String getMessage(MessageEvent<TextMessageContent> e) {
